004380ee0b0a0010b34c48bd960f3dd2c84d8856
unknown <Kostina13@gmail.com>
2022-02-18 10:21:19 +0300

    dz modul-7

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import video from './Deep.mp4';
+import Visiblescreen from './Visiblescreen'
+import loupe from './loupe-32.jpg';
+import './App.css';
+
+function App() {
+
+  const API_ID = "9a06e7d8" ;
+  const API_KEY = "c0f0d0dbcadcc6be297cd71709b9b6f1"; 
+  
+  const [mySearch, setMySearch] = useState('');
+  const [myRecipe, setMyResipe] = useState([]);
+  const [wordSubmit, setWordSubmit] = useState('ice-cream');
+
+  useEffect (async ()=> {
+const responce = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmit}&app_id=${API_ID}&app_key=${API_KEY}`);
+const data = await responce.json();
+console.log(data.hits)
+setMyResipe(data.hits)
+  }, [wordSubmit])
+
+  const yourSearch = (e) =>{
+    setMySearch(e.target.value);
+  }
+
+  const fimalSearch = (e) =>{
+    e.preventDefault();
+    setWordSubmit(mySearch)
+  }
+
+  return (
+    <div className="App">
+      <div className='container-video'>
+        <video autoPlay muted loop>
+        <source src={video} type='video/mp4'/>
+        </video>
+      </div>
+        <h1>Recipes for every day</h1>
+    <div className="container">
+        <form onSubmit={fimalSearch}>
+          <input onChange={yourSearch} placeholder='Напиши блюдо или ингредиенты...' ></input>      
+        </form>  
+        <button><img src={loupe} onClick={fimalSearch} alt='icon' className='input'/></button>
+    </div>
+  <div className='foremap'>
+      {myRecipe.map(item =>(
+        <Visiblescreen
+        label={item.recipe.label}
+        image = {item.recipe.image}
+        ingredients = {item.recipe.ingredientLines}
+        caloriesAll = {item.recipe.calories}
+        weigthAll = {item.recipe.totalWeight}
+        proteinAll = {item.recipe.digest[2].total}
+        carbonAll = {item.recipe.digest[1].total}
+        fatAll = {item.recipe.digest[0].total}
+        />
+))}
+  </div>
+   </div>    
+  );
+}
+
+export default App;
+
+
---
+++ src/Visiblescreen.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+function Visiblescreen ({label, image, ingredients, caloriesAll, weigthAll, proteinAll, fatAll, carbonAll}){
+
+    const [detaly, setDetaly] = useState(true);
+        const total = (caloriesAll.toFixed() /weigthAll.toFixed()) * 100;
+        const carbon = (carbonAll / weigthAll)*100;
+        const fat = (fatAll / weigthAll)*100;
+        const protein = (proteinAll / weigthAll)*100;
+
+        const handleClick = () =>{
+            setDetaly(!detaly);
+        }
+
+    return(
+    <div  className='card' >
+        <h2>{label} </h2>
+        <img src={image} alt='pict'/>
+        <button onClick={()=>handleClick()} className="btn" >More details</button>
+   {detaly? ("") : (
+    <div id='detali'> 
+    <div className="detali" >
+        <h3 className="header">Recipe and Ingredients</h3>
+        <ul>
+            {ingredients.map (ingredient =>(
+            <li>{ingredient}</li>    
+            ))}
+        </ul>
+        </div>
+        <h4>Nutritional value per 100 gr</h4>
+        <div className="table">
+<p className="calory">Calories, kcal:     {total.toFixed()}</p>
+<p className="fat">Proteins, g:    {protein.toFixed()}</p>
+<p className="fat">Fat, g:    {fat.toFixed()} </p>
+<p className="fat">Carbohydrates, g:     {carbon.toFixed()}</p>
+    </div>
+</div>
+   )}
+</div>
+    )
+}
+
+export default Visiblescreen;
+
